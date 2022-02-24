d35218b23228fee4edc2ce6bbe92d09ab3c16491
Klaus Vancamelbeke <klaus@proles.be>
2022-02-24 16:31:54 +0100

    Downgrade to Core v1.10.5 and Graphics to v1.0.0

--- src/WroverDisplayTest/Program.cs
+++ src/WroverDisplayTest/Program.cs
@@ -10,34 +10,20 @@ namespace WroverDisplayTest
     {
         public static void Main()
         {
-            const int backLightPin = 5;
-            const int chipSelect = 22;
-            const int dataCommand = 21;
-            const int reset = 18;
-            const int screenWidth = 320;
-            const int screenHeight = 240;
-
             Configuration.SetPinFunction(25, DeviceFunction.SPI1_MISO);
             Configuration.SetPinFunction(23, DeviceFunction.SPI1_MOSI);
             Configuration.SetPinFunction(19, DeviceFunction.SPI1_CLOCK);
 
-            DisplayControl.Initialize(
-                new SpiConfiguration(1, chipSelect, dataCommand, reset, backLightPin),
-                new ScreenConfiguration(0, 0, screenWidth, screenHeight), 2 * 1024 * 1024);
-
             Bitmap fullScreenBitmap = DisplayControl.FullScreen;
             Font font = Resources.GetFont(Resources.FontResources.DefaultFont);
             Color color = ColorUtility.ColorFromRGB(255, 0, 0);
-            Color white = ColorUtility.ColorFromRGB(255, 255, 255);
-
-            fullScreenBitmap.Clear();
-            fullScreenBitmap.Flush();
 
             while (true)
             {
+                fullScreenBitmap.Clear();
                 fullScreenBitmap.DrawText($"Time is {DateTime.UtcNow:HH:mm:ss}", font, color, 30, 10);
                 fullScreenBitmap.Flush();
-                Thread.Sleep(500);
+                Thread.Sleep(100);
             }
         }
     }
